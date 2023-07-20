f7119b857744563c6bee049ca3fe273424c05c19
vladislawovk <vladislawovk@gmail.com>
2023-07-20 18:45:39 +0200

    Create data, domain and presentation packages

--- app/src/main/java/com/vladislawovk/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/vladislawovk/shoppinglist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vladislawovk.shoppinglist
+package com.vladislawovk.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vladislawovk.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
