9308db2d135a6804862eab3f468df33fde7de110
vladislawovk <vladislawovk@gmail.com>
2023-07-25 16:57:17 +0200

    Add default id

--- app/src/main/java/com/vladislawovk/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/vladislawovk/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -7,7 +7,12 @@ object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopList = mutableListOf<ShopItem>()
 
+    private var autoIncrementId = 0
+
     override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
         shopList.add(shopItem)
     }
 
