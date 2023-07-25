70aebd29046c2de0a03b78246918abeb6c0e5e60
vladislawovk <vladislawovk@gmail.com>
2023-07-25 16:20:21 +0200

    Add repository implementation

---
+++ app/src/main/java/com/vladislawovk/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,34 @@
+package com.vladislawovk.shoppinglist.data
+
+import com.vladislawovk.shoppinglist.domain.ShopItem
+import com.vladislawovk.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    override fun addShopItem(shopItem: ShopItem) {
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        shopList.add(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId }
+            ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+
+}
